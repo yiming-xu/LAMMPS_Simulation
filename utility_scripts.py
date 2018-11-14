80ce81d87aaa532a7568b56f6e15a784e05f0d0b
Yiming Xu <d.artume@gmail.com>
2018-11-14 13:04:39 +0000

    added replace_molecule utility

--- utility_scripts.py
+++ utility_scripts.py
@@ -4,7 +4,48 @@
 # This is a list of scripts that are somewhat useful
 from ase import Atoms, units
 from ase.build import molecule
+import numpy as np
 from numpy.random import rand
+from ase.data import *
+from ase.neighborlist import neighbor_list
+
+def identify_connection(i, j, num, connections = None):
+    'Given a neighbor_list i, j, identifies all indices connected to num'
+    if not connections:
+        connections = [num]
+        
+    new_connections = j[i == num]
+    to_check = [x for x in new_connections if x not in connections]
+    connections.extend(to_check)
+    
+    for n in to_check:
+        identify_connection(i, j, n, connections)
+        
+    return connections
+    
+def replace_molecule(sim_box, source_index, new_mol, cut_off=1.0, seed=None):
+    'Replaces a molecule attached to an atom at source_index of the sim_box and replace it by new_mol'
+    new_box = sim_box.copy()
+    old_pbc = new_box.get_pbc()
+    
+    overlap_vdwr_sphere = [vdw_radii[atomic_numbers[x]] for x in new_box.get_chemical_symbols()]
+    overlap_covr_sphere = [covalent_radii[atomic_numbers[x]] for x in new_box.get_chemical_symbols()]
+    overlap_sphere = cut_off*(np.array(overlap_vdwr_sphere)+np.array(overlap_covr_sphere))/2
+    
+    i,j = neighbor_list('ij', new_box, overlap_sphere, self_interaction=False)
+    
+    mol_to_delete_indices = identify_connection(i, j, num = source_index)
+    mol_to_delete_COM = new_box[mol_to_delete_indices].get_center_of_mass()
+    
+    #print(mol_to_delete_indices)
+    
+    new_mol.translate(mol_to_delete_COM)
+    del new_box[mol_to_delete_indices]
+    
+    new_box.extend(new_mol)
+    new_box.set_pbc(old_pbc)
+    
+    return new_box
 
 def create_water_region(cell):
     'Creates a region of water with approximate density at 300K'
